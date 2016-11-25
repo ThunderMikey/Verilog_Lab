85401fcf796b88ed0aab97f86580113bf0db78ee
Alex Luisi <al3515@eews305-051.ic.ac.uk>
2016-11-25 19:39:19 +0000

    ex7 working

--- Part_2/ex7/ex7_lfsr.v
+++ Part_2/ex7/ex7_lfsr.v
@@ -1,14 +1,14 @@
 //7 bit LFSR with polynomial: 1 + X + X^7
 module LFSR_7(clock, outnext);
 	input clock;
-	output [6:0] outnext;
-	reg [6:0] outputnext;
+	output [7:1] outnext;
+	reg [7:1] outputnext;
 	
 	initial outputnext = 7'b1;
 	
 	always @ (posedge clock)
-
-		outputnext <= {outputnext[6:1], (outputnext[6] ^ outputnext[0])};
-	
+		begin
+		outputnext <= {outputnext[6:1], (outputnext[7] ^ outputnext[1])};
+		end
 	assign outnext = outputnext;
 endmodule
--- Part_2/ex7/ex7_top.v
+++ Part_2/ex7/ex7_top.v
@@ -1,12 +1,12 @@
 module ex7_top(KEY, HEX0, HEX1);
 	//input CLOCK_50;
-	input [3:0] KEY;
+	input [3:3] KEY;
 	output [6:0] HEX0, HEX1;
 	
 	wire [6:0] prbsout;
 	//wire [3:0] seg7_0, seg7_1, seg7_2;
 	
-	LFSR_7 lfsr(KEY[3], prbsout);
+	LFSR_7 lfsr(KEY, prbsout);
 	//bin2bcd8 binstage({1'b0,prbsout}, seg7_0, seg7_1, seg7_2);
 	hex_to_7seg hexstage0 (HEX0, prbsout[3:0]);
 	hex_to_7seg hexstage1 (HEX1, {1'b0, prbsout[6:4]});
